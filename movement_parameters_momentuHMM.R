9977f9a1c19b7c58068d2e1e0b8f56cfa8a1b8d2
bibianadasoler <bibianadasoler@gmail.com>
2023-03-30 22:36:30 +0100

    reduzi o script fazendo a conversao das coord com dplyr

--- movement_parameters_momentuHMM.R
+++ movement_parameters_momentuHMM.R
@@ -1,4 +1,4 @@
-# movement parameters - step size and turning angle
+# script to estimate movement parameters - step size and turning angle
 # based on Statistical Methods Series: Movement Ecology - Theo Michelot momentuHMM 
 
 # Packages and functions----
@@ -11,8 +11,8 @@ library(adehabitatLT)
 library(momentuHMM)
 source("tutorial_momentuHMM_utility_functions.R")
 
-# Importing and organizing data ----
-# importando os dados da telemetria e selecionando registros de interesse
+# Data - loading and organizing ----
+# importando os dados da telemetria originais e selecionando registros de interesse
 #telemetria <- data %>%
   as_tibble() %>%
   filter(!ID %in% c("Yoki", 'Phoenix 1 error')) %>% # exclude animals with gps problem
@@ -20,31 +20,23 @@ source("tutorial_momentuHMM_utility_functions.R")
   mutate(time = as.POSIXct(Acquisition.Time, format = "%Y.%m.%d %H:%M:%S", tz = "America/New_York"))  %>%
   mutate(ID2 = 1:n()) %>% #add column to identify each record
   dplyr::select(ID, GPS.Longitude, GPS.Latitude, time, ID2)
-# salvando os dados selecionados
-#write.table(telemetria, "gps_data_filtered.txt", row.names = F)
+# salvando os dados selecionados 
+#write.table(telemetria, "gps_data_filtered.csv", row.names = F)
 
+# carregando os dados filtrados acima
 telemetria <- read.csv("gps_data_filtered.csv") %>% 
-  mutate(time = as.POSIXct(time, format = "%Y-%m-%d %H:%M:%S", tz = "America/New_York")) %>%
-  group_by(ID) %>%
-  mutate(diff = c(0, diff(time))) %>%
-  filter(diff <= 0 | diff >= 18) %>% # remove records obtained in a time interval < 18 min, but 0 is kept to mantain the first record
-  dplyr::select(ID, GPS.Longitude, GPS.Latitude, time, ID2)
+  mutate(time = as.POSIXct(time, format = "%Y-%m-%d %H:%M:%S", tz = "America/New_York")) %>% # transformando a coluna com a hora do registro no formato POSIXct
+  group_by(ID) %>% # agrupando os dados por ID
+  mutate(diff = c(0, diff(time))) %>% # calculando a diferenca de tempo entre registros do mesmo ID
+  filter(diff <= 0 | diff >= 18) %>% ungroup() %>% # remove records obtained in a time interval < 18 min, but 0 is kept to maintain the first record
+  st_as_sf(., coords = c("GPS.Longitude", "GPS.Latitude"), crs = CRS("+proj=longlat +datum=WGS84")) %>% # define as coords como longlat
+  st_transform(., crs = CRS("+proj=aea +lat_0=-32 +lon_0=-60 +lat_1=-5 +lat_2=-42 +x_0=0 +y_0=0 +ellps=aust_SA +units=m +no_defs")) %>% # South America Albers Equal Area Conic
+  mutate(x = st_coordinates(.)[,1],
+         y = st_coordinates(.)[,2]) %>% # criando colunas com as coordenadas x e y
+  as.data.frame(.) %>%  # transformando para dataframe
+  dplyr::select(ID, x, y, time, ID2) # selecionando colunas de interesse
 
 #visualizing data
-telemetria %>%
-  filter(ID == "Ben") %>%
-ggplot(., aes(GPS.Longitude, GPS.Latitude, col = ID)) +
-  geom_point(size = 0.1) + geom_path(linewidth = 0.1)
-
-# Project to UTM
-llcoord <- st_as_sf(telemetria[, c("GPS.Longitude", "GPS.Latitude")], coords = c("GPS.Longitude", "GPS.Latitude"), 
-                    crs = CRS("+proj=longlat +datum=WGS84"))
-utmcoord <- st_transform(llcoord, crs = CRS("+proj=aea +lat_0=-32 +lon_0=-60 +lat_1=-5 +lat_2=-42 +x_0=0 +y_0=0 +ellps=aust_SA +units=m +no_defs"))
-
-# Add Easting-Northing to data 
-telemetria[, c("x", "y")] <- st_coordinates(utmcoord)
-
-# Plot Northing vs Easting
 telemetria %>%
   filter(ID == "Ben") %>%
 ggplot(., aes(x, y, col = ID)) + 
@@ -52,14 +44,9 @@ ggplot(., aes(x, y, col = ID)) +
   coord_equal()
 
 # Use function from utility_function.R to split data at gaps 1 hour
-telemetria_split <-  split_at_gap(data = telemetria, max_gap = 60, shortest_track = 60)
+telemetria_split <- split_at_gap(data = telemetria, max_gap = 60, shortest_track = 60)
 
-telemetria_split %>%
-  group_by(ID) %>%
-  mutate(diff = c(0, diff(time))) %>%
-  with(., (table(diff)))
-
-# visualizing
+# visualizing data com e sem split_at_gap
 telemetria %>%
   filter(ID == "Ben") %>%
   ggplot(., aes(x, y, col = ID)) + 
@@ -83,17 +70,22 @@ telemetria_adehabitat <- setNA(ltraj = as.ltraj(xy = telemetria_split[, c("x", "
 telemetria_na <- ld(telemetria_adehabitat)[, c("id", "x", "y", "date")]
 colnames(telemetria_na) <- c("ID", "x", "y", "time")
 
+telemetria_na %>%
+  group_by(ID) %>%
+  mutate(diff = c(0, diff(time))) %>% ungroup() %>% summarise(mean(diff))
+
+# Model ----
 # Prepare data for HMM (compute step lengths and turning angles)
 telemetria_hmm <-prepData(telemetria_na, type = "UTM")
 
 head(telemetria_hmm, 10)
 
 # step length 
-hist(telemetria_hmm$step, breaks = 100, xlim = c(0, 1000))
+hist(telemetria_hmm$step, breaks = 100, xlim = c(0, 100))
 
 telemetria_hmm %>%
   filter(str_detect(ID, "Annie"),
-         step >= 20)  %>%
+         step >= 30)  %>%
   ggplot(., aes(x, y, col = ID)) + 
   geom_point(size = 0.1) + geom_path(linewidth = 0.1) + theme(legend.position="none") +
   coord_equal()
@@ -104,7 +96,7 @@ hist(telemetria_hmm$angle, breaks = seq(-pi, pi, length = 15))
 # Observation distributions (step lengths and turning angles)
 dist <- list(step = "gamma", angle = "vm")
 
-# Initial parameters
+# Defining initial parameters to fit the model
 # A short guide to choosing initial parameter values for the estimation in moveHMM -Theo Michelot & Roland Langrock
 # We can therefore select one starting value near the lower end of the range of observed step lengths (for state 1, say), and the other starting value such that the corresponding state can capture the steps which we would intuitively associate with more active behaviour (for state 2).
 # When the gamma distribution is used to model step lengths, the standard deviation is usually of the same order of magnitude as the mean of the distribution
@@ -122,13 +114,14 @@ hmm3s_tamandua <- fitHMM(telemetria_hmm, nbStates = 3, dist = dist, Par0 = Par_3
 # Print parameter estimates
 hmm2s_tamandua
 hmm3s_tamandua
+AIC(hmm2s_tamandua, hmm3s_tamandua)
 
 # Plot estimated distributions and state-coloured tracks
-plot_hmm2s <- plot(hmm2s_tamandua, breaks = 25, ask = FALSE)
-alex1 <- plot(hmm2s_tamandua, animals = "Alexander-1", breaks = 25, ask = FALSE)
-plot_hmm3s <- plot(hmm3s_tamandua, breaks = 25, ask = FALSE)
+plot(hmm2s_tamandua, which = 1, ask = FALSE)
+plot(hmm2s_tamandua, animals = "Alexander-1", plotCI = T, ask = FALSE)
+plot(hmm3s_tamandua, breaks = 25, ask = FALSE)
 
 # Plot pseudo-residuals for 2-state and 3-state models
-PRhmm2s <- plotPR(hmm2s_tamandua)
-PRhmm3s <- plotPR(hmm3s_tamandua)
+plotPR(hmm2s_tamandua)
+plotPR(hmm3s_tamandua)
 
