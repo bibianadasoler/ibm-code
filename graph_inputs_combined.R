265d267d28b4404537cfaab2e3fbe09eb88ffa22
Bibiana Terra Dasoler <84736570+bibianadasoler@users.noreply.github.com>
2023-11-29 17:56:42 -0300

    uploading

--- graph_inputs_combined.R
+++ graph_inputs_combined.R
@@ -1,3 +1,150 @@
+<<<<<<< HEAD
+##
+## Article: Wildlife road crossings are not everywhere: a theoretical approach 
+## for maximizing mitigation
+## doi: 
+#
+## Script to create figures for to visually explore the relationship between 
+## model results and all the parameters combined
+#
+#
+# loading ggplot
+library(ggplot2)
+
+# Habitat amount model ----
+# load file with simulations information
+habitat_amount <- readRDS(here::here("results", "habitat_amount_simulations.RDS"))
+habitat_amount_simulations <- habitat_amount@simdesign@simoutput
+
+# categorizing variables
+habitat_categoric <- habitat_amount_simulations %>%
+  dplyr::mutate(class_prop = dplyr::case_when(proportion_of_habitat >= 10 & proportion_of_habitat < 30 ~ "10-30",
+                                              proportion_of_habitat >= 30 & proportion_of_habitat < 50 ~ "30-50",
+                                              proportion_of_habitat >= 50 & proportion_of_habitat < 70 ~ "50-70",
+                                              proportion_of_habitat >= 70 & proportion_of_habitat <= 90 ~ "70-90"),
+                class_matrix = dplyr::case_when(matrix_permeability >= 10 & matrix_permeability < 30 ~ "a10-30",
+                                                matrix_permeability >= 30 & matrix_permeability < 50 ~ "b30-50",
+                                                matrix_permeability >= 50 & matrix_permeability < 70 ~ "c50-70",
+                                                matrix_permeability >= 70 & matrix_permeability <= 90 ~ "d70-90"),
+                class_vision = dplyr::case_when(vision_angle >= 90 & vision_angle < 120 ~ "a90-120",
+                                                vision_angle >= 120 & vision_angle < 150 ~ "b120-150",
+                                                vision_angle >= 150 & vision_angle <= 180 ~ "c150-180")) %>%
+  dplyr::select(assess_top_sections, total_crossings, class_prop, class_matrix, perceptual_range, class_vision) 
+
+# to plot results for crossings aggregation
+(habitat_aggreg <- ggplot(habitat_categoric, aes(perceptual_range, assess_top_sections, col = class_prop)) +
+  annotate("rect", xmin = -Inf, xmax = Inf,  ymin = 0.25, ymax = 1, alpha = 0.15, fill = "grey15") +
+  annotate("rect", xmin = -Inf, xmax = Inf,  ymin = 0.5, ymax = 1, alpha = 0.15, fill = "grey15") +
+  annotate("rect", xmin = -Inf, xmax = Inf,  ymin = 0.75, ymax = 1, alpha = 0.15, fill = "grey15") +
+  geom_hline(yintercept = 0.25, linetype = "dashed", color = "black") +
+  geom_smooth() +
+  scale_color_manual(values = c("#d7301f", "#b30000", "#7f0000", "black")) +
+  scale_y_continuous(n.breaks = 5, limits = c(0,1), sec.axis = sec_axis(~., name = "Matrix permeability", breaks = NULL)) +
+  scale_x_continuous(sec.axis = sec_axis(~ . , name = "Vision angle", breaks = NULL)) +
+  facet_grid(class_matrix ~ class_vision, 
+             labeller = labeller(class_vision = c("a90-120" = "90-120", "b120-150" = "120-150", "c150-180" = "150-180"),
+                                 class_matrix = c("a10-30" = "10-30", "b30-50" = "30-50", "c50-70" = "50-70", "d70-90" = "70-90")))  +
+  theme_bw() +
+  labs(title = "Habitat amount model", tag = "A", x = "Perceptual range", y ="Crossings aggregation", col = "Proportion of habitat") +
+  theme(legend.position = "bottom",
+        plot.tag.position = c(0.02, 0.99),
+        text = element_text(size = 12),
+        legend.text = ggtext::element_markdown(size = 11),
+        axis.title.y.right = element_text(vjust = 1),
+        axis.title.x.top = element_text(vjust = .4)))
+
+# to plot results for total crossings 
+(habitat_total <- ggplot(habitat_categoric, aes(perceptual_range, total_crossings, col = class_prop)) +
+  geom_smooth() +
+  scale_color_manual(values = c("#d7301f", "#b30000", "#7f0000", "black")) +
+  scale_y_continuous(breaks = c(0, 750, 1500), limits = c(0,1500), sec.axis = sec_axis(~., name = "Matrix permeability", breaks = NULL)) +
+  scale_x_continuous(sec.axis = sec_axis(~ . , name = "Vision angle", breaks = NULL)) +
+  facet_grid(class_matrix ~ class_vision, 
+             labeller = labeller(class_vision = c("a90-120" = "90-120", "b120-150" = "120-150", "c150-180" = "150-180"),
+                                 class_matrix = c("a10-30" = "10-30", "b30-50" = "30-50", "c50-70" = "50-70", "d70-90" = "70-90")))  +
+  theme_bw() +
+  labs(title = "Habitat amount model", tag = "A", x = "Perceptual range", y ="Total crossings", col = "Proportion of habitat") +
+  theme(legend.position = "bottom",
+        plot.tag.position = c(0.02, 0.99),
+        text = element_text(size = 12),
+        legend.text = ggtext::element_markdown(size = 11),
+        axis.title.y.right = element_text(vjust = 1),
+        axis.title.x.top = element_text(vjust = .4)))
+
+
+# Habitat arrangement model ----
+# load file with simulations information
+habitat_arrangement <- readRDS(here::here("results", "habitat_arrangement_simulations.RDS"))
+habitat_arrangement_simulations <- habitat_arrangement@simdesign@simoutput
+
+# categorizing variables
+arrang_categoric <- habitat_arrangement_simulations %>%
+  dplyr::mutate(class_matrix = dplyr::case_when(matrix_permeability >= 10 & matrix_permeability < 30 ~ "a10-30",
+                                  matrix_permeability >= 30 & matrix_permeability < 50 ~ "b30-50",
+                                  matrix_permeability >= 50 & matrix_permeability < 70 ~ "c50-70",
+                                  matrix_permeability >= 70 & matrix_permeability <= 90 ~ "d70-90"),
+         class_vision = dplyr::case_when(vision_angle >= 90 & vision_angle < 120 ~ "a90-120",
+                                  vision_angle >= 120 & vision_angle < 150 ~ "b120-150",
+                                  vision_angle >= 150 & vision_angle <= 180 ~ "c150-180"),
+         scenario = as.character(scenario)) %>%
+  dplyr::select(assess_top_sections, total_crossings, scenario, class_matrix, perceptual_range, class_vision) 
+
+# to plot results for crossings aggregation
+(arrang_aggreg <- ggplot(arrang_categoric, aes(perceptual_range, assess_top_sections, colour = scenario)) +
+  annotate("rect", xmin = -Inf, xmax = Inf,  ymin = 0.25, ymax = 1, alpha = 0.15, fill = "grey15") +
+  annotate("rect", xmin = -Inf, xmax = Inf,  ymin = 0.5, ymax = 1, alpha = 0.15, fill = "grey15") +
+  annotate("rect", xmin = -Inf, xmax = Inf,  ymin = 0.75, ymax = 1, alpha = 0.15, fill = "grey15") +
+  geom_hline(yintercept = 0.25, linetype = "dashed", color = "black") +
+  geom_smooth() +
+  scale_color_manual(values = c("black", "#4575b4", "#74add1", "#fdae61", "#f46d43", "red", "#a50026"),
+                     labels = c("A", "B", "C", "D", "E", "F", "G")) +  
+  scale_y_continuous(n.breaks = 5, limits = c(0,1), sec.axis = sec_axis(~., name = "Matrix permeability", breaks = NULL)) +
+  scale_x_continuous(sec.axis = sec_axis(~ . , name = "Vision angle", breaks = NULL)) +
+  facet_grid(class_matrix ~ class_vision, 
+             labeller = labeller(class_vision = c("a90-120" = "90-120", "b120-150" = "120-150", "c150-180" = "150-180"),
+                                 class_matrix = c("a10-30" = "10-30", "b30-50" = "30-50", "c50-70" = "50-70", "d70-90" = "70-90")))  +
+  theme_bw() +
+  labs(title = "Habitat arrangement model", tag = "B", x = "Perceptual range", y ="Crossings aggregation", col = "Scenarios") +
+  theme(legend.position = "bottom",
+        plot.tag.position = c(0.02, 0.99),
+        text = element_text(size = 12),
+        legend.text = ggtext::element_markdown(size = 11),
+        axis.title.y.right = element_text(vjust = 1),
+        axis.title.x.top = element_text(vjust = .4)) +
+  guides(colour = guide_legend(ncol = 7)))
+
+(arrang_total <- ggplot(arrang_categoric, aes(perceptual_range, total_crossings, col = scenario)) +
+  geom_smooth() +
+  scale_color_manual(values = c("black", "#4575b4", "#74add1", "#fdae61", "#f46d43", "red", "#a50026"),
+                     labels = c("A", "B", "C", "D", "E", "F", "G")) +  
+  scale_y_continuous(breaks = c(0, 1800, 3600), limits = c(0,3600), sec.axis = sec_axis(~., name = "Matrix permeability", breaks = NULL)) +
+  scale_x_continuous(sec.axis = sec_axis(~ . , name = "Vision angle", breaks = NULL)) +
+  facet_grid(class_matrix ~ class_vision, 
+             labeller = labeller(class_vision = c("a90-120" = "90-120", "b120-150" = "120-150", "c150-180" = "150-180"),
+                                 class_matrix = c("a10-30" = "10-30", "b30-50" = "30-50", "c50-70" = "50-70", "d70-90" = "70-90")))  +
+  theme_bw() +
+  labs(title = "Habitat arrangement model", tag = "B", x = "Perceptual range", y ="Total crossings", col = "Scenarios") +
+  theme(legend.position = "bottom",
+        plot.tag.position = c(0.02, 0.99),
+        text = element_text(size = 12),
+        legend.text = ggtext::element_markdown(size = 11),
+        axis.title.y.right = element_text(vjust = 1),
+        axis.title.x.top = element_text(vjust = .4)) +
+  guides(colour = guide_legend(ncol = 7)))
+
+### panel combining plots of crossings aggregation from both models
+(aggreg <- patchwork::wrap_elements(habitat_aggreg) / patchwork::wrap_elements(config_aggreg))
+# saving
+ggsave(aggreg, filename = here::here("figures", "combined_inputs_aggregation.png"), 
+       dpi = 300, width = 3200, height = 4200, unit = "px")
+
+
+### panel combining plots of total crossings from both models
+(total <- patchwork::wrap_elements(habitat_total) / patchwork::wrap_elements(config_total))
+# saving
+ggsave(total, filename = here::here("figures", "combined_inputs_total.png"), 
+       dpi = 300, width = 3200, height = 4200, unit = "px")
+=======
 library(here)
 library(dplyr)
 library(ggplot2)
@@ -130,3 +277,4 @@ ggsave(aggreg, filename = here::here("imagens", "inputs_aggregation.png"),
 total <- wrap_elements(habitat_total) / wrap_elements(config_total)
 ggsave(total, filename = here::here("imagens", "inputs_total.png"), 
        dpi = 300, width = 3200, height = 4200, unit = "px")
+>>>>>>> a6440e5850e1344c6b7da7ca8ec79541993a403e
--- sensitivity_analysis_habitat_amount.R
+++ sensitivity_analysis_habitat_amount.R
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 ##
 ## Article: Wildlife road crossings are not everywhere: a theoretical approach 
 ## for maximizing mitigation
@@ -112,3 +113,119 @@ require(ggplot2)
                                              plot.title = element_text(size = 12, vjust = 1))))
 
 ## go to script "habitat_arrangment_sensitivity_analysis to combine and save figures
+=======
+##
+## Article: Wildlife road crossings are not everywhere: a theoretical approach 
+## for maximizing mitigation
+## doi: 
+#
+## Script to run sensitivity analysis using the files created in the script to 
+## run the simulations "simulations_habitat_amount."
+## Model: Habitat amount
+#
+
+# load file with simulations information
+habitat_amount_simulations <- readRDS(here::here("results", "habitat_amount_simulations.rds"))
+
+# create objects for input samples
+random_sample_1_amount <- habitat_amount_simulations@simdesign@simobject[[1]][["X1"]]
+random_sample_2_amount <- habitat_amount_simulations@simdesign@simobject[[1]][["X2"]]
+experiment_design_amount <- habitat_amount_simulations@simdesign@siminput
+
+# create objects for outputs
+aggregation_values_amount <- habitat_amount_simulations@simdesign@simoutput$assess_top_sections
+crossings_values_amount <- habitat_amount_simulations@simdesign@simoutput$total_crossings
+
+### Sensitivity analysis ----
+
+#### Crossings aggregation output ----
+sensitivity_aggreg_amount <- sensitivity::sobol2007(model = NULL, X1 = random_sample_1_amount, X2 = random_sample_2_amount, nboot = 300) 
+sensitivity_aggreg_amount$X <- experiment_design_amount
+sensitivity::tell(sensitivity_aggreg_amount, (aggregation_values_amount-mean(aggregation_values_amount))/sd(aggregation_values_amount))
+print(sensitivity_aggreg_amount)
+
+# organizing data from first and total effect from sobol to plot
+aggreg_first_graph_amount <- sensitivity_aggreg_amount[["S"]] %>%
+  dplyr::mutate(parameter = row.names(.),
+                index = "first-order")
+aggreg_total_graph_amount <- sensitivity_aggreg_amount[["T"]] %>%
+  dplyr::mutate(parameter = row.names(.),
+                index = "total")
+aggreg_graph_amount <- rbind(aggreg_first_graph_amount, aggreg_total_graph_amount)
+
+# plot sensitivity results
+require(ggplot2)
+(sensi_aggreg_amount <- ggplot(aggreg_graph_amount, aes(x = original, y = parameter,  group = index)) +
+    geom_pointrange(aes(xmin = `min. c.i.`, xmax = `max. c.i.`, color = index), 
+                    position = position_dodge(width = 0.3), size = 0.3) +
+    scale_colour_manual(values = c("first-order" = "black", "total" = "grey50"),
+                        labels = c("Main effect (*S<sub>i</sub>*)", "Total effect (*S<sub>Ti</sub>*)"), name = " ") +
+    labs(title = "Crossings aggregation", y = "Parameters", x = "Sobol Index", tag = "A") +
+    scale_x_continuous(limits = c(0, 1)) +
+    scale_y_discrete(limits = c("perceptual_range", "vision_angle", "proportion_of_habitat", "matrix_permeability"),
+                     labels=c("vision_angle" = "Vision angle", "proportion_of_habitat" = "Proportion of habitat",
+                              "perceptual_range" = "Perceptual range", "matrix_permeability" = "Matrix permeability")) +
+    theme(panel.background = element_rect(fill = "white"),
+          legend.key = element_rect(fill = "white"),
+          axis.line = element_line(color = "black"),
+          panel.grid.major.y = element_line(colour = "grey99"),
+          legend.position = "bottom",
+          axis.text = element_text(size = 12),
+          axis.title = element_text(size = 12),
+          legend.text = ggtext::element_markdown(size = 11),
+          plot.title = element_text(size = 12),
+          plot.tag = element_text(size = 11),
+          plot.tag.position = c(0.01,0.99))
+)
+
+#### Total crossings output ----
+sensitivity_crossings_amount <- sensitivity::sobol2007(model = NULL, X1 = random_sample_1_amount, X2 = random_sample_2_amount, nboot = 300) 
+sensitivity_crossings_amount$X <- experiment_design_amount
+sensitivity::tell(sensitivity_crossings_amount, (crossings_values_amount-mean(crossings_values_amount))/sd(crossings_values_amount))
+print(sensitivity_crossings_amount)
+
+# organizing data from first and total effect from sobol to plot
+crossings_first_graph_amount <- sensitivity_crossings_amount[["S"]] %>%
+  dplyr::mutate(parameter = row.names(.),
+                index = "first-order")
+crossings_total_graph_amount <- sensitivity_crossings_amount[["T"]] %>%
+  dplyr::mutate(parameter = row.names(.),
+                index = "total")
+crossings_graph_amount <- rbind(crossings_first_graph_amount, crossings_total_graph_amount) %>%
+  dplyr::mutate(`min. c.i.` = ifelse(`min. c.i.` <= 0, 0, `min. c.i.`),
+                original = ifelse(original <= 0, 0, original))
+
+# plot sensitivity results
+require(ggplot2)
+(sensi_cross_amount <- ggplot(crossings_graph_amount, aes(x = original, y = parameter,  group = index)) +
+    geom_pointrange(aes(xmin = `min. c.i.`, xmax = `max. c.i.`, color = index), 
+                    position = position_dodge(width = 0.3), size = 0.3) +
+    scale_colour_manual(values = c("first-order" = "black", "total" = "grey50"),
+                        labels = c("Main effect (*S<sub>i</sub>*)", "Total effect (*S<sub>Ti</sub>*)"), name = " ") +
+    labs(title = "Total crossings", y = "Parameters", x = "Sobol Index", tag = "B") +
+    scale_x_continuous(limits = c(0, 1)) +
+    scale_y_discrete(limits = c("vision_angle", "matrix_permeability",  "proportion_of_habitat", "perceptual_range"),
+                     labels=c("vision_angle" = "Vision angle", "proportion_of_habitat" = "Proportion of habitat",
+                              "perceptual_range" = "Perceptual range", "matrix_permeability" = "Matrix permeability")) +
+    theme(panel.background = element_rect(fill = "white"),
+          legend.key = element_rect(fill = "white"),
+          axis.line = element_line(color = "black"),
+          panel.grid.major.y = element_line(colour = "grey99"),
+          legend.position = "bottom",
+          axis.text = element_text(size = 12),
+          axis.title = element_text(size = 12),
+          legend.text = ggtext::element_markdown(size = 11),
+          plot.title = element_text(size = 12),
+          plot.tag = element_text(size = 11),
+          plot.tag.position = c(0.01,0.99))
+)
+
+# combining the two plots of habitat amount:
+(habitat_amount <- sensi_aggreg_amount + sensi_cross_amount + 
+    patchwork::plot_layout(ncol = 2, guides = "collect") +
+    patchwork::plot_annotation(title = 'Habitat amount model', 
+                               theme = theme(legend.position = 'none', 
+                                             plot.title = element_text(size = 12, vjust = 1))))
+
+## go to script "habitat_arrangment_sensitivity_analysis to combine and save figures
+>>>>>>> a6440e5850e1344c6b7da7ca8ec79541993a403e
